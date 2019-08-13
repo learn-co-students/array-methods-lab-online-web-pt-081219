e562f850f3a594ea8dc8a94c41171ba307641b0d
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-13 06:34:23 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,44 @@
 def using_include(array, element)
+  array.include?(element)
 
 end
+ using_include([1,2,3,4,5], 4)
+#----------------------------------------
+
 
 def using_sort(array)
-
+ array.sort 
 end
+
+ using_sort([1,5,3,8,2])
+#----------------------------------------------
+
 
 def using_reverse(array)
-
+ array.reverse
 end
+
+ using_reverse([1,2,3,4,5])
+#----------------------------------------------
+
 
 def using_first(array)
-
+ array.first 
 end
+
+ using_first([1,2,3,4,5])
+#----------------------------------------------
+
 
 def using_last(array)
-
+  array.last 
 end
+
+using_last([1,2,3,4,5])
+#----------------------------------------------
 
 def using_size(array)
-
+  array.size 
 end
+
+using_size([1,2,3,4,5])
