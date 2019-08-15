47ef0aa50d43b24bd4b58bd48451a2b8431e65ad
clean-command-1851 <ed7901979t@gmail.com>
2019-08-15 19:00:11 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,33 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+  array.first
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
-
+  array.size
 end
+
+array = ["one", "two", "three", "four"]
+using_include(array, "five" )
+using_sort(array)
+using_reverse(array)
+using_first(array)
+using_last(array)
+using_size(array)
+
+
